a743732c6f3d3232ef1dc4ecf06d0e14827016e2
Marcus <iths@Apples-MacBook-Pro-7.local>
2016-04-30 11:28:19 +0200

    safetysave

---
+++ youCanDiEt/PopoverEditDeleteViewController.h
@@ -0,0 +1,13 @@
+//
+//  PopoverEditDeleteViewController.h
+//  youCanDiEt
+//
+//  Created by Marcus on 2016-04-30.
+//  Copyright © 2016 Marcus Cehlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopoverEditDeleteViewController : UIViewController
+
+@end
---
+++ youCanDiEt/PopoverEditDeleteViewController.m
@@ -0,0 +1,28 @@
+//
+//  PopoverEditDeleteViewController.m
+//  youCanDiEt
+//
+//  Created by Marcus on 2016-04-30.
+//  Copyright © 2016 Marcus Cehlin. All rights reserved.
+//
+
+#import "PopoverEditDeleteViewController.h"
+
+@interface PopoverEditDeleteViewController ()
+
+@end
+
+@implementation PopoverEditDeleteViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+
+@end
