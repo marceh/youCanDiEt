d676a5229f4091e4c58f800c60a2ec1e43eaecaf
Marcus <iths@Apples-MacBook-Pro-7.local>
2016-05-01 13:06:13 +0200

    fixed all logical parts, i think... NS is to style the shit...

--- youCanDiEt/RecipesClickedViewController.m
+++ youCanDiEt/RecipesClickedViewController.m
@@ -14,13 +14,15 @@
 
 @property (weak, nonatomic) PARMananger *parManager;
 @property (weak, nonatomic) Recipe *theRecipe;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *viewInsideScrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageViewPic;
 @property (weak, nonatomic) IBOutlet UILabel *labelName;
 @property (weak, nonatomic) IBOutlet UILabel *labelKcal;
-@property (weak, nonatomic) IBOutlet UILabel *labelCarbs;
-@property (weak, nonatomic) IBOutlet UILabel *labelProtein;
-@property (weak, nonatomic) IBOutlet UILabel *labelFat;
-@property (weak, nonatomic) IBOutlet UILabel *labelDietValue;
+@property (weak, nonatomic) IBOutlet UITableView *theTableView;
+@property (weak, nonatomic) IBOutlet UITextView *textViewHowTo;
+@property (weak, nonatomic) IBOutlet UILabel *labelKcalNotportion;
+
 
 @end
 
@@ -32,11 +34,9 @@
     self.theRecipe = self.parManager.comparableRecipeOne;
     self.imageViewPic.image = [UIImage imageWithContentsOfFile:[self.theRecipe getTheRightFolderAndImagePath]];
     self.labelName.text = self.theRecipe.name;
-    self.labelKcal.text = [NSString stringWithFormat:@"Total Kcal: %@", [self.theRecipe.dictionaryOfRecipeContent valueForKey:@"kcal"]];
-    self.labelCarbs.text = [NSString stringWithFormat:@"Total Carbs: %@", [self.theRecipe.dictionaryOfRecipeContent valueForKey:@"carbs"]];
-    self.labelProtein.text = [NSString stringWithFormat:@"Total Protein: %@", [self.theRecipe.dictionaryOfRecipeContent valueForKey:@"protein"]];
-    self.labelFat.text = [NSString stringWithFormat:@"Total Fat: %@", [self.theRecipe.dictionaryOfRecipeContent valueForKey:@"fat"]];
-    self.labelDietValue.text = [NSString stringWithFormat:@"%d", [self.theRecipe getDietValue]];
+    self.labelKcal.text = [NSString stringWithFormat:@"Portions: %@",self.theRecipe.portions];
+    self.labelKcalNotportion.text = [NSString stringWithFormat:@"Kcals/portion: %@",[self.theRecipe getKcalsPerPortions]];
+    self.textViewHowTo.text = self.theRecipe.howTo;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,6 +49,27 @@
     } else {
         [self performSegueWithIdentifier:@"FromRecipesClickedToRecipes" sender:self];
     }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.theRecipe.products.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellInTableviewInScrollView"];
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cellInTableviewInScrollView"];
+    
+    cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:12];
+    cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica" size:8];
+    
+    cell.textLabel.text = [[self.theRecipe.products[indexPath.row] valueForKey:@"product"] valueForKey:@"name"];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Recipe contains %@ grams of this product.",[[self.theRecipe.products[indexPath.row] valueForKey:@"grams"] stringValue]];
+    
+    return cell;
 }
 
 @end
